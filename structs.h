edd517888d83e89f8b909b356168cf946d87d9bf
DevJhonatanUD <jdmorenob@correo.udistrital.edu.co>
2022-11-17 22:29:17 -0500

    structs and library modified

--- structs.h
+++ structs.h
@@ -4,32 +4,57 @@
 #include "lista.h"
 
 using namespace std;
-struct tarjeta{
+
+struct estadio{
+	string nombreEstadio;
+	int capacidad;
+	string ciudad;
+};
+
+struct equipo{
+	string nombre;
+	string entrenador;
+	string confederacion;
 	
-	string color;
-	int dia;
-	int mes;
-	int anio;
-	string motivo;
-	string contrincante;	
+//	Lista<jugador> j;
+};
+
+struct programacion{
+	estadio estadio1;
+	equipo equipo1;
+	equipo equipo2;
+	string hora;
+		
 };
 
 struct jugador{
 	string nombre;
+	string nacionalidad;
+	string fechaDeNacimiento;
+	int estatura;
+	float edad;
+	string club;
 	string posicion;
-	int edad;
-	string talla;
-	Lista <tarjeta> t;
+	int golesAnotados;
+	int fechasSancion; //Se almacena N fechas Fifa
 };
 
 
 //
-//struct equipo{
-//	string nombre;
-//	 string entrenador;
-//	 string confederacion;
-//	 Lista<jugador> j;
-//};
+
+
+struct primeraFase{
+	char grupo;
+	List<equipo> e;
+	List<programacion> p;
+};
+
+struct segundaFase{
+	programacion pr;
+	List<equipo> e; //Solo dos equipos
+	
+};
+
 
 
 
